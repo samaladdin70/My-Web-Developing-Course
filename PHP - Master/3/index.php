aa50909cd9745bd713a2dc51708363bdd860119b
Aladdin Sami <samaladdin70@gmail.com>
2023-01-08 02:08:31 +0200

    finish lesson 3 php

---
+++ PHP - Master/3/index.php
@@ -0,0 +1,52 @@
+<?php
+/* 
+data type:
+string
+integer : -1 0 1 2 3
+float : 2.23
+bolean : true false
+null
+array
+object
+resources
+
+*/
+/* assign operator = */
+$x = "Hello Programming";
+$y = 15;
+$y = $y + 5;
+echo $y;
+
+/* arithmatic operators */
+// is equal ==
+// is identical ===
+// != not equal
+// !== not identical
+// > greater than
+// >= greater than or equal
+// < less than 
+// <= less than or equal
+
+//echo $y;
+/* indexed array */
+//$fruits = array('Banana', 'Apple', 'Orange');
+//$fruits = ['Banana', 'Apple', 'Orange'];
+// echo 2;
+//echo $fruits[2];
+
+/* Associative array */
+
+//$car = array('color' => 'red', 'mani' => 2022, 'type' => 'BMW');
+
+/* echo $car['color'];
+echo "<br>";
+$bb = true; */
+//var_dump($bb);
+
+?>
+<!-- <pre style="width:200px; background-color:black;color:yellow;padding:5px; border-radius:5px;">
+    <?php
+    var_dump($car);
+
+    ?>
+</pre> -->
